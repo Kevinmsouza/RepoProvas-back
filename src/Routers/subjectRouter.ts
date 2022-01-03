8035d35a3e9bf3ce589f2cfc966c996ebbd460fe
ligia.bozzi@gmail.com <ligia@respondeai.com.br>
2022-01-03 09:05:09 -0300

    preparing review

--- src/Routers/subjectRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as subjectController from '../controllers/subjectController';
-
-const router = Router();
-
-router.get('', subjectController.listSubjects);
-
-export default router;
--- src/Routers/testRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as testController from '../controllers/testController';
-
-const router = Router();
-
-router.post('', testController.createTest);
-router.get('', testController.listTestsByCategory);
-
-export default router;
